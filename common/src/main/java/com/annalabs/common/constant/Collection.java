4eaf28eec29f3e53ba4e95ed087b391cc665fe7d
Victor Shneer <victor.shneer@gmail.com>
2025-03-04 16:19:41 +0200

    implement asset

--- common/src/main/java/com/annalabs/common/constant/Collection.java
+++ common/src/main/java/com/annalabs/common/constant/Collection.java
@@ -2,4 +2,6 @@ package com.annalabs.common.constant;
 
 public class Collection {
     public static final String projectCollection = "Project";
+    public static final String assetCollection = "Asset";
+
 }
